8e8af0381e810b1626367f9d7ce5d7868463f697
akshitgarg4 <akshit2000garg@gmail.com>
2021-10-20 21:07:53 +0530

    Authentication Problem Fixed

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const port = 8000;
-// const passportJWT=require('./config/passport-jwt');
+const db = require("./config/mongoose");
+const passportJWT = require("./config/passport-jwt");
 const app = express();
-// app.use(express.urlencoded());
-// app.get("/", function (req, res) {
-//   console.log("request received");
-//   return res.status(401).json({ message: "Welcome to CodeZone from Backend" });
-// });
+app.use(express.urlencoded());
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
