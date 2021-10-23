67b3cb18066df210a9aca48e61c6040073fd7d4c
Shayan <shayan.yaseen1@gmail.com>
2021-10-24 02:41:03 +0530

    Added dark mode t

--- frontend/src/Components/StudentDashboard.js
+++ frontend/src/Components/StudentDashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { fetchUserClassDetails,clearUserClassDetails } from "../actions/classroom";
 import { Link } from 'react-router-dom';
+import { Paper,Button,Grid } from '@mui/material';
 
 class StudentDashboard extends Component {
   componentDidMount() {
@@ -14,22 +15,32 @@ class StudentDashboard extends Component {
     //create the details of the classes joined by the user
     const { classesJoined } = this.props.classroom;
     return (
-      <div>
-        <b>Student Dashboard</b><br>
-        </br><br></br>
-        <b>Classes Joined</b><br></br>
-        {classesJoined.map((classroom) => (
-          <div className='classroom' key={classroom._id}>
-            Subject - {classroom.subject}<br></br>
-            Batch  - {classroom.batch}<br></br>
-            Description - {classroom.description}<br></br>
-            Classroom code - {classroom.code}<br></br>
-            CreatedBy - {classroom.creator.name}<br></br>
-            Students Enrolled - {classroom.students.length}<br></br>
-            <div className="field"><Link to={`/classroom/${classroom._id}`}><button> Enter Classroom</button></Link></div>
-          </div>
-        ))}
-      </div>
+      <Paper>
+        <div>
+          <Grid container direction="column">
+          <Button variant="contained" color="primary">
+          This is a button
+          </Button>
+          <Button variant="contained" color="secondary">
+          This is another button
+          </Button>
+          </Grid>
+          <b>Student Dashboard</b><br>
+          </br><br></br>
+          <b>Classes Joined</b><br></br>
+          {classesJoined.map((classroom) => (
+            <div className='classroom' key={classroom._id}>
+              Subject - {classroom.subject}<br></br>
+              Batch  - {classroom.batch}<br></br>
+              Description - {classroom.description}<br></br>
+              Classroom code - {classroom.code}<br></br>
+              CreatedBy - {classroom.creator.name}<br></br>
+              Students Enrolled - {classroom.students.length}<br></br>
+              <div className="field"><Link to={`/classroom/${classroom._id}`}><button> Enter Classroom</button></Link></div>
+            </div>
+          ))}
+        </div>
+      </Paper>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,19 @@
+import { Paper } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./Components/App";
+import App from "./components/App";
 import "./index.css";
 import { configureStore } from "./store";
-import {ThemeProvider} from '@mui/material';
-import theme from './customization/theme';
 
 const store = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
+      <Paper>
         <App />
-      </ThemeProvider>
+      </Paper>
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
