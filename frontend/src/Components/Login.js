b59ea4fae4bf53eb02abb24a4dca21b6ff90f1d9
Gaganpreet Khurana <giftykhurana0@gmail.com>
2021-11-05 20:07:10 +0530

    Login page done

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -3,75 +3,139 @@ import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { login, clearAuth } from "../actions/auth";
 
+// Material UI
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme();
+
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        };
+    }
+
+    //to clear the error if it comes on reload or whenever the user shifts from this page
+    componentWillUnmount() {
+        this.props.dispatch(clearAuth());
+    }
+
+    handleSubmitForm = (e) => {
+        e.preventDefault();
+        const { email, password } = this.state;
+        if (email && password) {
+            this.props.dispatch(login(email, password));
+        }
     };
-  }
-  
-  //to clear the error if it comes on reload or whenever the user shifts from this page
-  componentWillUnmount() {
-    this.props.dispatch(clearAuth());
-  }
+    handleEmail = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    };
+    handlePassword = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+    };
+    render() {
+        const { inProgress, error, isLoggedIn } = this.props.auth;
+        if (isLoggedIn) {
+            return <Redirect to = "/" / > ;
+        }
+        return (
 
-  handleSubmitForm = (e) => {
-    e.preventDefault();
-    const { email, password } = this.state;
-    if (email && password) {
-      this.props.dispatch(login(email, password));
-    }
-  };
-  handleEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
-  handlePassword = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
-  render() {
-    const { inProgress, error, isLoggedIn } = this.props.auth;
-    if (isLoggedIn) {
-      return <Redirect to="/" />;
-    }
-    return (
-      <form className="login-form">
-        <span className="login-signup-header">LOG IN</span>
-        {error && <div className="alert error-dailog">{error}</div>}
-        <div className="field">
-          <input
-            type="email"
-            placeholder="Email"
-            required
-            onChange={this.handleEmail}
-          />
-        </div>
-        <div className="field">
-          <input
-            type="password"
-            placeholder="password"
-            required
-            onChange={this.handlePassword}
-          />
-        </div>
-        <div className="field">
-          <button onClick={this.handleSubmitForm} disabled={inProgress}>
-            Log In
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
-function mapStateToProps(state) {
-  return {
-    auth: state.auth,
-  };
-}
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                marginTop: 8,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Sign in
+                            </Typography>
+                            {error && <div className="alert error-dailog">{error}</div>}
+                            <Box component="form" onSubmit={this.handleSubmitForm} noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    onChange={ this.handleEmail }
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    onChange={ this.handlePassword }
+                                />
+                                {/*<FormControlLabel*/}
+                                {/*    control={<Checkbox value="remember" color="primary" />}*/}
+                                {/*    label="Remember me"*/}
+                                {/*/>*/}
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    disabled={ inProgress }
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    {/*<Grid item xs>*/}
+                                    {/*    <Link href="#" variant="body2">*/}
+                                    {/*        Forgot password?*/}
+                                    {/*    </Link>*/}
+                                    {/*</Grid>*/}
+                                    <Grid item>
+                                        <Link href="/signup" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Container>
 
-export default connect(mapStateToProps)(Login);
+                );
+        }
+    }
+
+    function mapStateToProps(state) {
+        return {
+            auth: state.auth,
+        };
+    }
+
+    export default connect(mapStateToProps)(Login);
