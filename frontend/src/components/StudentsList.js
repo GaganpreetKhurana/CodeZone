b6e4bcbf55a3d097e6dde57986e4ff8384329053
akshitgarg4 <akshit2000garg@gmail.com>
2021-11-15 14:05:26 +0530

    Notice board created and buttons to join meet and lab displayed

--- frontend/src/components/StudentsList.js
+++ frontend/src/components/StudentsList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import ChatBox from "./ChatBox";
 
 class StudentsList extends React.Component {
   render() {
@@ -59,51 +60,7 @@ class StudentsList extends React.Component {
                     </div>
                 </div>
             </div>
-            <div id="user-chat-box">
-                <div className="chat-header">
-                    <span> 18103001 ABCDEFG</span>
-                    <span className="close">X</span>
-                </div>
-		        <ul id="chat-messages-list">
-			        <li class="other-message">
-				        <span>Other Message</span>
-			        </li>
-			        <li className="self-message">
-				        <span>
-					        Self Message
-				        </span>
-			        </li>
-                    <li class="other-message">
-				        <span>Other Message</span>
-			        </li>
-			        <li className="self-message">
-				        <span>
-					        Self Message
-				        </span>
-			        </li>
-                    <li class="other-message">
-				        <span>Other Message</span>
-			        </li>
-			        <li className="self-message">
-				        <span>
-					        Self Message
-				        </span>
-			        </li>
-                    <li class="other-message">
-				        <span>Other Message</span>
-			        </li>
-			        <li className="self-message">
-				        <span>
-					        Self Message
-				        </span>
-			        </li>
-
-		        </ul>
-		        <div id="chat-message-input-container">
-			        <input id="chat-message-input" placeholder="Type message here"/>
-			        <button id="send-message">Send</button>
-		        </div>
-            </div>
+            {/* <ChatBox/> */}
       </div>
     );
   }
