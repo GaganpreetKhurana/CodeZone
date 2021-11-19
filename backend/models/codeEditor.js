371fab4a934257be704cebb4230261db0dd4b52f
akshitgarg4 <akshit2000garg@gmail.com>
2021-11-19 14:45:03 +0530

    Schema for codeEditor  added

---
+++ backend/models/codeEditor.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const codeEditorSchema = new mongoose.Schema({
+    content: { type: String },
+    lab: { type: mongoose.Schema.Types.ObjectId, ref: "Lab" },
+    customInput: { type: String },
+    customOutput: { type: String },
+    languageSelected:{type:String},
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+}, { timestamps: true });
+const codeEditor = mongoose.model("codeEditor", codeEditorSchema);
+module.exports = codeEditor;
