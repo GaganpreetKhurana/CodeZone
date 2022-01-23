70c08c983cb4f2bcab910e3b5e39fea99f27eb78
Gaganpreet Khurana <giftykhurana0@gmail.com>
2022-01-23 17:05:00 +0530

    Minor Fix

--- frontend/src/components/Classroom.js
+++ frontend/src/components/Classroom.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React , { Component } from "react";
 import { connect } from "react-redux";
 import DiscussionPortal from "./DiscussionPortal";
 import StudentsList from "./StudentsList";
@@ -6,80 +6,77 @@ import NoticeBoard from "./NoticeBoard";
 
 //Material UI
 import { Grid } from '@mui/material';
-import { clearClassroomDetails, fetchClassroomDetails } from "../actions/classroom";
+import { clearClassroomDetails , fetchClassroomDetails } from "../actions/classroom";
 
 class Classroom extends Component {
-    componentDidMount() {
+    componentDidMount () {
         const { match } = this.props;
-
-        if (match.params.classroomID) {
-            this.props.dispatch(fetchClassroomDetails(match.params.classroomID));
-            this.timer = setInterval(() => {
-                this.props.dispatch(fetchClassroomDetails(match.params.classroomID));
-            }, 5000);
+        
+        if ( match.params.classroomID ) {
+            this.props.dispatch ( fetchClassroomDetails ( match.params.classroomID ) );
+            this.timer = setInterval ( () => {
+                this.props.dispatch ( fetchClassroomDetails ( match.params.classroomID ) );
+            } , 5000 );
         }
     }
-
-    componentDidUpdate(prevProps) {
+    
+    componentDidUpdate ( prevProps ) {
         const {
-            match: { params: prevParams },
+            match : { params : prevParams } ,
         } = prevProps;
         const {
-            match: { params: currentParams },
+            match : { params : currentParams } ,
         } = this.props;
-        if (prevParams && currentParams && currentParams !== prevParams) {
+        if ( prevParams && currentParams && currentParams !== prevParams ) {
             //fetch new classroom  details
-            this.props.dispatch(fetchClassroomDetails(currentParams.classroomID));
+            this.props.dispatch ( fetchClassroomDetails ( currentParams.classroomID ) );
         }
     }
-
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-        this.props.dispatch(clearClassroomDetails());
+    
+    
+    componentWillUnmount () {
+        clearInterval ( this.timer );
+        this.props.dispatch ( clearClassroomDetails () );
     }
-    render() {
+    
+    render () {
         const { match } = this.props;
         // return <div>{match.params.classroomID}</div>;
-        return ( <
-            Grid container direction = "row"
-            justifyContent = "space-evenly" >
-            <
-            Grid item xs = { 6 }
-            m = { 2 }
-            style = {
-                { maxHeight: "150vh", overflow: "auto" }
-            } > { " " } <
-            DiscussionPortal classroomId = { match.params.classroomID }
-            /> < /
-            Grid > <
-            Grid item xs = { 4 }
-            m = { 2 }
-            container direction = "column"
-            justifyContent = "space-evenly"
-            alignItems = "stretch" >
-            <
-            Grid item m = { 2 } >
-            <
-            NoticeBoard classroomId = { match.params.classroomID }
-            />{" "} < /
-            Grid > <
-            Grid item m = { 2 } >
-            <
-            StudentsList classroomId = { match.params.classroomID }
-            /> < /
-            Grid > <
-            /Grid> < /
-            Grid >
+        return ( <Grid container direction="row" justifyContent="space-evenly">
+            <Grid
+                item
+                xs={6}
+                m={2}
+                style={{ maxHeight: "150vh", overflow: "auto" }}
+            >
+                {" "}
+                <DiscussionPortal classroomId={match.params.classroomID} />
+            </Grid>
+            <Grid
+                item
+                xs={4}
+                m={2}
+                container
+                direction="column"
+                justifyContent="space-evenly"
+                alignItems="stretch"
+            >
+                <Grid item m={2}>
+                    <NoticeBoard classroomId={match.params.classroomID} />{" "}
+                </Grid>
+                <Grid item m={2}>
+                    <StudentsList classroomId={match.params.classroomID} />
+                </Grid>
+            </Grid>
+        </Grid>
         );
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps ( state ) {
     return {
-        auth: state.auth,
-        createClassroom: state.createClassroom,
+        auth : state.auth , createClassroom : state.createClassroom ,
     };
 }
 
-export default connect(mapStateToProps)(Classroom);
+export default connect ( mapStateToProps ) ( Classroom );
