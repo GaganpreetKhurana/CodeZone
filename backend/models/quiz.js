d41384dc3e628a9f3a49c1c4ee63bdd41fc2b689
Gaganpreet Khurana <giftykhurana0@gmail.com>
2022-02-16 01:51:17 +0530

    Quiz Model Created

---
+++ backend/models/quiz.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+var QuizSchema = new mongoose.Schema({
+	class: {
+		type: mongoose.Schema.Types.ObjectId, ref: "Class"
+	},
+	questions: [{
+		question: {type: String},
+		options: [{
+			type: String
+		}],
+		correctOption: {type: Number, default: 0},
+		questionType: {type: String, default: "mcq"},
+		maxScore: {type: Number},
+		studentAnswers: [{
+			student: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+			optionSelected: {type: Number, default: - 1},
+			marksScored: {type: Number, default: 0},
+		}]
+		
+	}],
+	dateScheduled: {
+		type: Date, default: Date.now
+	}
+	creator: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+	
+}, {timestamps: true});
+
+const Quiz = mongoose.model("Quiz", QuizSchema);
+module.exports = Quiz
