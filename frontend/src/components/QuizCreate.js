16b97247e2ef35734a7438b0ed5e94aaab3aa705
Shayan <shayan.yaseen1@gmail.com>
2022-02-15 18:01:04 +0530

    Checkpoint

--- frontend/src/components/QuizCreate.js
+++ frontend/src/components/QuizCreate.js
@@ -2,16 +2,48 @@ import React from "react";
 import { connect } from "react-redux";
 
 //Material UI
-import { Grid } from "@mui/material";
+import { Grid, Box ,Card ,Paper } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
 class QuizCreate extends React.Component {
   render() {
     return (
       <div>
-        <Grid container direction="column" height="100vh">
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        </Grid>
+        <Paper height="100vh">
+          <Box
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <Card
+              sx={{
+                minWidth: "100vh",
+                minHeight: "70vh",
+                bgcolor: (theme) =>
+                  theme.palette.mode === "dark" ? "#272727" : "#fff",
+                boxShadow: (theme) =>
+                  theme.palette.mode === "dark"
+                    ? "unset"
+                    : "0 8px 16px 0 #BDC9D7",
+              }}
+            >
+              <Grid container justifyContent="space-evenly" alignItems="center">
+                <Grid item m={4} xs={12} sm={12} md={12}>
+                  {" "}
+                  <TextField
+                    id="filled-basic"
+                    label="Filled"
+                    variant="filled"
+                    fullWidth="True"
+                  />
+                </Grid>
+              </Grid>
+            </Card>
+          </Box>
+        </Paper>
       </div>
     );
   }
