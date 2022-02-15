081a1bcdd21be452357c7bb9945f74f98cce5bb2
Gaganpreet Khurana <giftykhurana0@gmail.com>
2022-02-16 03:21:03 +0530

    Quiz Update and delete apis created

--- backend/controllers/api/quiz.js
+++ backend/controllers/api/quiz.js
@@ -1,5 +1,7 @@
 const Quiz =require("../../models/quiz")
 const Class =require("../../models/class")
+const sanitizer = require ( 'sanitizer' )
+
 
 module.exports.create = async function ( req , res ) {
 	console.log ( req.body,"RR" );
@@ -17,7 +19,7 @@ module.exports.create = async function ( req , res ) {
 		
 		// user is teacher for this subject
 		
-		//create announcement object
+		//create quiz object
 		let newQuiz = await Quiz.create ( {
 			creator : req.user._id ,
 			questions : req.body.questions ,
@@ -62,4 +64,122 @@ module.exports.create = async function ( req , res ) {
 			message : "User is not a teacher in class!" ,
 		} );
 	}
+}
+
+//to delete an quiz
+module.exports.delete = async function ( req , res ) {
+	
+	console.log ( req.params.quiz_id );
+	// get quiz
+	let quiz = await Quiz.findById ( sanitizer.escape ( req.params.quiz_id ) );
+	if ( !quiz ) {
+		return res.status ( 404 ).json ( {
+			success : false ,
+			message : "Quiz not found!" ,
+		} );
+	}
+	// get subject
+	let subject = await Class.findById ( quiz.class._id );
+	if ( !subject ) {
+		return res.status ( 404 ).json ( {
+			success : false ,
+			message : "Subject not found!" ,
+		} );
+	}
+	
+	
+	if ( subject.teachers.includes ( req.user._id ) ) {
+		
+		// user is a teacher for this subject
+		
+		subject.quizzes.pop ( quiz )
+		subject = await subject.save ();
+		Quiz.findByIdAndDelete ( quiz._id ).exec ();
+		
+		return res.status ( 200 ).json ( {
+			message : "Quiz deleted!" ,
+			success : true ,
+			data : await Class.findById (
+					req.body.classroom_id )
+				.select ( "quizzes" )
+				.populate ( {
+					path : "quizzes" ,
+					populate : {
+						path : "creator" ,
+						select : "name" ,
+					} ,
+					options : {
+						sort : { createdAt : - 1 }
+					}
+				} ) ,
+		} );
+	} else {
+		return res.status ( 401 ).json ( {
+			success : false ,
+			message : "User is not a teacher in this class!" ,
+		} );
+	}
+	
+}
+
+//to update an quiz
+module.exports.update = async function ( req , res ) {
+	
+	
+	// get quiz
+	let quiz = await Quiz.findById ( req.body.quiz_id );
+	if ( !quiz ) {
+		return res.status ( 404 ).json ( {
+			success : false ,
+			message : "Quiz not found!" ,
+		} );
+	}
+	
+	// get subject
+	let subject = await Class.findById ( quiz.class._id );
+	if ( !subject ) {
+		return res.status ( 404 ).json ( {
+			success : false ,
+			message : "Subject not found!" ,
+		} );
+	}
+	
+	
+	if ( subject.teachers.includes ( req.user._id ) && quiz.creator._id == req.user._id ) {
+		
+		// user is a teacher for this subject
+		quiz.questions = req.body.questions ,
+		quiz.dateScheduled = req.body.dateScheduled,
+		quiz = await quiz.save ();
+		
+		return res.status ( 200 ).json ( {
+			message : "Quiz Updated!" ,
+			success : true ,
+			data : await Class.findById (
+					req.body.classroom_id )
+				.select ( "quizzes" )
+				.populate ( {
+					path : "quizzes" ,
+					populate : {
+						path : "creator" ,
+						select : "name" ,
+					} ,
+					options : {
+						sort : { createdAt : - 1 }
+					}
+				} ) ,
+		} );
+	} else {
+		if ( !subject.teachers.includes ( req.user._id ) ) {
+			return res.status ( 401 ).json ( {
+				success : false ,
+				message : "User is not a teacher in this class!" ,
+			} );
+		}
+		return res.status ( 401 ).json ( {
+			success : false ,
+			message : "User did not create this quiz!" ,
+		} );
+	}
+	
 }
--- backend/routes/api/quiz.js
+++ backend/routes/api/quiz.js
@@ -4,8 +4,8 @@ const quiz = require("../../controllers/api/quiz");
 const auth = require("../../config/authenticate");
 
 router.post("/create", auth.authenticateToken, quiz.create);
-// router.get ( '/delete::announcement_id' , auth.authenticateToken , announcements.delete );
-// router.post ( '/update' , auth.authenticateToken , announcements.update );
+router.get ( '/delete::quiz_id' , auth.authenticateToken , quiz.delete );
+router.post ( '/update' , auth.authenticateToken , quiz.update );
 
 
 module.exports = router;
