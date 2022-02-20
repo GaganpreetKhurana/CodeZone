45f6f07253201170863c6b27d6a8e4ead8658b95
Gaganpreet Khurana <giftykhurana0@gmail.com>
2022-02-20 23:45:11 +0530

    Models Edited
    Signed-off-by: Gaganpreet Khurana <giftykhurana0@gmail.com>

--- backend/models/quiz.js
+++ backend/models/quiz.js
@@ -5,36 +5,18 @@ var QuizSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId, ref: "Class"
 	},
 	questions: [{
-		question: {type: String,required: true},
-		options: [{
-			type: String
-		}],
-		correctOption: [{type: Number, default: 0}],
-		questionType: {type: String, default: "single_correct"},
-		maxScore: {type: Number, default:0},
-		studentAnswers: [{
-			student: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-			optionSelected: {type: Number, default: - 1},
-			marksScored: {type: Number, default: 0},
-		}]
+		type: mongoose.Schema.Types.ObjectId, ref: "Question"
 	}],
 	maxScoreQuiz: {
 		type: Number, default: function(){
 			total = 0;
-			for(let i = 0; i < this.questions.length;i ++)
-			{
+			for(let i = 0; i < this.questions.length; i++){
 				total += this.questions[i].maxScore;
 			}
 			return total;
 		},
-		required : true
+		required: true
 	},
-	submissions: [{
-		student: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-		submittedAt: {type: Date},
-		marksScored: {type: Number, default: 0},
-		
-	}],
 	dateScheduled: {
 		type: Date, default: Date.now
 	},
