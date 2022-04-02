e3f9c95733b002b588ebc82a4cebbac35e905dc6
Shayan <shayan.yaseen1@gmail.com>
2022-04-03 00:13:26 +0530

    grid fix

--- frontend/src/components/Video_Conferencing/App2.js
+++ frontend/src/components/Video_Conferencing/App2.js
@@ -1,23 +1,48 @@
 import { useState } from "react";
 import { Button } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import VideoCall from "./VideoCall";
 
 function App2() {
   const [inCall, setInCall] = useState(false);
-
   return (
     <div className="App" style={{ height: "100%" }}>
-      {inCall ? (
-        <VideoCall setInCall={setInCall} />
-      ) : (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setInCall(true)}
-        >
-          Join Call
-        </Button>
-      )}
+      <Grid
+        container
+        spacing={24}
+        justify="center"
+        alignItems="column"
+        style={{ minHeight: "100vh", maxWidth: "100%" }}
+      >
+        {inCall ? (
+          <Grid
+            item
+            m={2}
+          >
+            <VideoCall setInCall={setInCall} />
+            </Grid>
+        ) : (
+          <Grid
+            item
+            xs={3}
+            align="center"
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setInCall(true)}
+            >
+              Join Call
+            </Button>
+          </Grid>
+        )}
+      </Grid>
     </div>
   );
 }
